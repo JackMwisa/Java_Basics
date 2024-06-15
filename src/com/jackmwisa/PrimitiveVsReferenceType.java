3229dfbbcd016b369f47074fda8f926552fbf742
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-15 17:48:39 +0300

    Strings in java

--- src/com/jackmwisa/PrimitiveVsReferenceType.java
+++ src/com/jackmwisa/PrimitiveVsReferenceType.java
@@ -18,6 +18,8 @@ public class PrimitiveVsReferenceType {
         Point point1 = new Point(1,2);
         Point point2 = point1;
 
+        point1.x = 3;
+        System.out.println(point2);
 
 
     }
