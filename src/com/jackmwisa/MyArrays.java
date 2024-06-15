7d6732d4c64b8088a5cd09853ee4cd09dbb5cb67
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-15 18:11:29 +0300

    Update MyArrays.java

--- src/com/jackmwisa/MyArrays.java
+++ src/com/jackmwisa/MyArrays.java
@@ -4,7 +4,7 @@ public class MyArrays {
     public static void main(String[] args) {
         int[] number = {10,5,6,8,9,15,20,3,7};
 
-        Arrays.to
+        Arrays.toString()
         System.out.println(number);
 
     }
