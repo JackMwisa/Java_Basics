acc7dbea2933eeceb8dadb092944fb71b4dd103a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-15 18:27:04 +0300

    constants in java

---
+++ src/com/jackmwisa/ContantsInJava.java
@@ -0,0 +1,12 @@
+package com.jackmwisa;
+
+public class ContantsInJava {
+    //constants in java
+    public static void main(String[] args) {
+        final float pi = 3.14f;
+
+//        pi += .4;
+        System.out.println(pi);
+
+    }
+}
