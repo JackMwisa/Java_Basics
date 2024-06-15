781b033e87026767fcdc3f96dce3931c4edaf9ef
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-15 18:34:12 +0300

    Create ArithmeticOps.java

---
+++ src/com/jackmwisa/ArithmeticOps.java
@@ -0,0 +1,15 @@
+package com.jackmwisa;
+
+public class ArithmeticOps {
+    public static void main(String[] args) {
+        int total = 10 + 3;
+        int product = 10 * 3;
+        int difference = 10 - 3;
+        double quotient = (double)10 / (double)3;
+
+        System.out.println("Total: " + total);
+        System.out.println("Product: " + product);
+        System.out.println("Difference: " + difference);
+        System.out.println("Quotient: " + quotient);
+    }
+}
