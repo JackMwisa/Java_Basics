64b20dff36941a9c201efccf3e660a66bb483677
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-15 18:22:40 +0300

    multidimentional arrays in Java

--- src/com/jackmwisa/MyArrays.java
+++ src/com/jackmwisa/MyArrays.java
@@ -1,11 +1,14 @@
 package com.jackmwisa;
-import java.util.Arrays
+import java.util.Arrays;
 public class MyArrays {
     public static void main(String[] args) {
         int[] number = {10,5,6,8,9,15,20,3,7};
 
-        Arrays.toString()
-        System.out.println(number);
+        System.out.println(number.length);
+
+        Arrays.sort(number);
+
+        System.out.println(Arrays.toString(number));
 
     }
 }
