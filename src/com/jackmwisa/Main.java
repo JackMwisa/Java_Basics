4a5c85181683819c11aa969819b01d97801f3fd7
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-15 17:22:14 +0300

    solved

--- src/com/jackmwisa/Main.java
+++ src/com/jackmwisa/Main.java
@@ -1,14 +1,10 @@
 package com.jackmwisa;
 
 public class Main {
-
     public static void main(String[] args) {
     //comment
         System.out.println("Hello and welcome!");
-
         System.out.println("How are you?");
-
-
 
     }
 
--- src/com/jackmwisa/ReferenceType.java
+++ src/com/jackmwisa/ReferenceType.java
@@ -1,6 +1,13 @@
 package com.jackmwisa;
 
 public class ReferenceType {
-    
+    public static void main(String[] args){
+
+        byte age = 30;
+        System.out.println(age);
+
+        
+    }
+
 
 }
