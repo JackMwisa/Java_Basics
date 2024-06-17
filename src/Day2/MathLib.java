dc622006b748f6b2a14e747c54262fb4bc74e369
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-17 18:16:33 +0300

    Update MathLib.java

--- src/Day2/MathLib.java
+++ src/Day2/MathLib.java
@@ -17,7 +17,27 @@ public class MathLib {
 
         System.out.println("The difference between monday and tuesday lunch price is: " + lunhcpriceDifference);
 
-        
+        System.out.println("\n/////////////////\n");
+
+        int lunchPriceInEuro = 20;
+        int sandwichPrice = 3;
+        int remainingMoneyAfterBuyingAsManySandwichesAsPossible = lunchPriceInEuro % sandwichPrice;
+
+        int totalSandwiches = lunchPriceInEuro / sandwichPrice;
+
+        System.out.println("Our total lunch price is " + lunchPrice + ", after buying as many many Sandwiches as possible we have: " + remainingMoneyAfterBuyingAsManySandwichesAsPossible +
+                " euros  left." + " We bought " + totalSandwiches + " sandwiches");
+
+
+        System.out.println("\n/////////////////\n");
+
+        int priceOne = 45;
+        int priceTwo = 56;
+        int maxPrice = Math.max(priceOne, priceTwo);
+        int lowPrice = Math.min(priceOne, priceTwo);
+
+        System.out.println("The higher lunch price is: " + maxPrice);
+        System.out.println("The lower lunch price is: " + lowPrice);
 
 
 
