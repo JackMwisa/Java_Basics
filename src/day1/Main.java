7f5b7aceb7238c3b8b7fd7c6f58c3e581e8cdf7a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-17 14:34:08 +0300

    practice

---
+++ src/day1/Main.java
@@ -0,0 +1,28 @@
+package day1;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello World".toUpperCase());
+//        System.out.println("Hello World".replace("World", "Jack"));
+
+        exerciseOne();
+        exerciseTwo();
+        exerciseThree();
+
+    }
+
+
+    public static void exerciseOne() {
+        
+    }
+
+    public static void exerciseTwo() {
+
+    }
+
+    public static void exerciseThree() {
+
+    }
+
+
+}
