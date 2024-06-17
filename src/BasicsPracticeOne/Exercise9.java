b88a7704e4d497e90bfa93994e67a584e144ce46
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-17 21:30:18 +0300

    A program that swaps 2 numbers
    exercise 9

--- src/BasicsPracticeOne/Exercise9.java
+++ src/BasicsPracticeOne/Exercise9.java
@@ -2,6 +2,24 @@ package BasicsPracticeOne;
 
 public class Exercise9 {
     public static void main(String[] args){
-        
+       /*
+       A program that swaps 2 numbers
+        */
+
+        int num1 = 4;
+        System.out.println("Values of number 1: " + num1);
+        int num2 = 7;
+        System.out.println("Values of number 2: " + num2);
+
+        int num3 = num1;
+        num1 = num2;
+        num2 = num3;
+
+        System.out.println("Swapped values of number 1: " + num1);
+        System.out.println("Swapped values of number 2: " + num2);
+
+
+
+
     }
 }
