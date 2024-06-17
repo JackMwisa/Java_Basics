21e2e2eb7745d04d1dac56fb8a4c8396e6867045
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-17 18:57:38 +0300

    Create Exercise3.java

---
+++ src/Day2/Exercise3.java
@@ -0,0 +1,32 @@
+package Day2;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        exerciseOne();
+        exerciseTwo();
+        exerciseThree();
+        exerciseFour();
+    }
+
+    public static void exerciseOne(){
+        double value = 45;
+        double valueSquareRoot = Math.sqrt(value);
+        System.out.println("My value is:  " + value);
+        System.out.println("My value's square root is:  " + valueSquareRoot);
+
+    }
+
+    public static void exerciseTwo(){
+
+        
+
+    }
+
+    public static void exerciseThree(){
+
+    }
+
+    public static void exerciseFour(){
+
+    }
+}
