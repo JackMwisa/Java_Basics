3820224c592cfa818dc86570e3f2a81db53a7812
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-17 14:41:54 +0300

    done ex1

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -13,14 +13,19 @@ public class Main {
 
 
     public static void exerciseOne() {
-        
+        System.out.println("Hello (My name)");
+        System.out.println("Hello Jack");
     }
 
     public static void exerciseTwo() {
-
+        System.out.print("Hello ");
+        System.out.println("Jack");
     }
 
     public static void exerciseThree() {
+        System.out.print("This Should ");
+        System.out.print("All Appear ");
+        System.out.print("On The Same Line");
 
     }
 
