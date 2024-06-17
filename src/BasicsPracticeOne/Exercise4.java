81e8d7a94d3736078f47ef2eeabfe1380d24e839
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-17 19:53:35 +0300

    Create Exercise4.java

---
+++ src/BasicsPracticeOne/Exercise4.java
@@ -0,0 +1,27 @@
+package BasicsPracticeOne;
+
+import java.util.Scanner;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        /*
+
+        5. Write a Java program that takes two numbers as input and displays the product of two numbers.
+        Test Data:
+        Input first number: 25
+        Input second number: 5
+        Expected Output :
+        25 x 5 = 125
+
+         */
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int firstNumber = input.nextInt();
+        System.out.print("Input first number: ");
+        int secondNumber = input.nextInt();
+
+        System.out.println(firstNumber + " x " + secondNumber + " = " + (firstNumber * secondNumber));
+
+    }
+}
