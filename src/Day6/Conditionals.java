92e4f0e1fe3d3a9c9544e524bc744dd13f0ae4a1
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-20 15:58:12 +0300

    Update Conditionals.java

--- src/Day6/Conditionals.java
+++ src/Day6/Conditionals.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 public class Conditionals {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Name: ");
-        String name = input.nextLine();
+//        Scanner input = new Scanner(System.in);
+//        System.out.printf("Name: ");
+//        String name = input.nextLine().trim();
+//
+//        System.out.println("You are " + name);
 
-        System.out.println("You are " + name);
+
+        int x = 1;
+        double y = 1.0d;
+
+        System.out.println(x == y);
+
+
     }
 }
