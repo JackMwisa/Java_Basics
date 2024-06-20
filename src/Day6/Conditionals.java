1c748c7b48fa3b3b826fadbc8e710d2ceab5b786
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-20 15:53:54 +0300

    Create Conditionals.java

---
+++ src/Day6/Conditionals.java
@@ -0,0 +1,14 @@
+package Day6;
+
+import java.util.Scanner;
+
+public class Conditionals {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Name: ");
+        String name = input.nextLine();
+
+        System.out.println("You are " + name);
+    }
+}
