3fb1182789c36474b8b5c3e5994569cd54780d14
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-20 16:46:51 +0300

    switch statments

--- src/Day6/TernaryOp.java
+++ src/Day6/TernaryOp.java
@@ -4,8 +4,9 @@ public class TernaryOp {
     public static void main(String[] args) {
 
         int income = 120_000;
-
         String className = income > 100_000 ? "First" : "Economy";
+
+        System.out.println(className);
 
     }
 }
