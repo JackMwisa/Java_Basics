c73f11d1ae24385a73436b20d6c80de3e33b5ad4
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-06-21 11:44:44 +0300

    Update MyArrays.java

--- src/com/jackmwisa/MyArrays.java
+++ src/com/jackmwisa/MyArrays.java
@@ -1,7 +1,9 @@
 package com.jackmwisa;
 import java.util.Arrays;
+
 public class MyArrays {
     public static void main(String[] args) {
+        
         int[] number = {10,5,6,8,9,15,20,3,7};
 
         System.out.println(number.length);
