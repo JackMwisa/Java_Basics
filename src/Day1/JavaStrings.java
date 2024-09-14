b561dd9e8a6fb5011783ca200f09a5f299ff6688
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-14 18:03:00 +0300

    Strings in Java

---
+++ src/Day1/JavaStrings.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class JavaStrings {
+    public static void main(String[] args){
+        String greet = "Ciao";
+        System.out.println(greet);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println(alphabet);
+        System.out.println("The lenght of the Alphabet string is: " + alphabet.length());
+    }
+}
