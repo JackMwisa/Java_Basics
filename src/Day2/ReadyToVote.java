8372ad034a6253d11f684b65b1a221de41c51e9d
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-14 18:56:58 +0300

    Create ReadyToVote.java

---
+++ src/Day2/ReadyToVote.java
@@ -0,0 +1,11 @@
+package Day2;
+
+public class ReadyToVote {
+    public static void main(String[] args) {
+
+        int age = 17;
+        int leagalAge = 18;
+
+        System.out.println("Ready to vote is: " + (leagalAge <= age));
+    }
+}
