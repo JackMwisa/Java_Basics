3b90140b71bf5f2e8dde799995e732a83cdf5d4b
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-14 19:15:41 +0300

    Ternary Operator

---
+++ src/Day2/TernaryOperator.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        byte hour = 12;
+        String time = (hour == 12) ? "It is noon" : "It is not noon";
+
+        System.out.println(time);
+
+    }
+}
