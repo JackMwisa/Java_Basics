9b98d41529883cce4072ef7d0edd3192ea18ffe3
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-14 18:18:46 +0300

    Update StringConcatenation.java

--- src/Day1/StringConcatenation.java
+++ src/Day1/StringConcatenation.java
@@ -2,6 +2,8 @@ package Day1;
 
 public class StringConcatenation {
     public static void main(String[] args) {
-
+        String firstName = "John";
+        String lastName = "Peter";
+        System.out.println(firstName + " " + lastName);
     }
 }
