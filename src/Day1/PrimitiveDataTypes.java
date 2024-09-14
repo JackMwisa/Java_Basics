74edec291e1551bbcb8258af174f13df028e9722
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-14 17:21:56 +0300

    Update PrimitiveDataTypes.java

--- src/Day1/PrimitiveDataTypes.java
+++ src/Day1/PrimitiveDataTypes.java
@@ -3,13 +3,11 @@ package Day1;
 public class PrimitiveDataTypes {
     public static void main(String[] args) {
 
-
         //Primitive data types - includes byte, short, int, long, float, double, boolean and char
         // 1 byte	Stores whole numbers from -128 to 127
         byte studentNumberInMyClass = 67;
         //short	2 bytes	Stores whole numbers from -32,768 to 32,767
         short populationInMyNeighborhood = 10_456;
-
         //int	4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
         int bankAccountBalance = 2_345_234;
         //long	8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
@@ -23,7 +21,6 @@ public class PrimitiveDataTypes {
         //char	2 bytes	Stores a single character/letter or ASCII values
         char myChar = 'E';
 
-
         System.out.println(studentNumberInMyClass);
         System.out.println(populationInMyNeighborhood);
         System.out.println(bankAccountBalance);
@@ -32,7 +29,19 @@ public class PrimitiveDataTypes {
         System.out.println(myDecimal);
         System.out.println(isHappy);
         System.out.println(myChar);
-    }
 
+
+//      A floating point number can also be a scientific number with an "e" to indicate the power of 10:
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+
+
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+    }
 
 }
