95df94729bc0d706ab25d1699ca0b869a333b532
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-14 16:16:49 +0300

    Update FirstJavaProgram.java

--- src/Day1/FirstJavaProgram.java
+++ src/Day1/FirstJavaProgram.java
@@ -12,7 +12,20 @@ public class FirstJavaProgram {
 
         //using print method
         System.out.print("Hello");
-        System.out.print("Mark");
+        System.out.print("Mark\n\n");
+
+
+        //using println for numbers
+        // for numbers we do not use quotes
+        System.out.println(89);
+        System.out.println(132);
+        System.out.println(44532 + "\n");
+
+        //mathematical calculation in print method
+        System.out.println("addition: 7 + 2 = " + 7 + 2);
+        System.out.println("substraction: 7 - 3 = " + (7 - 3));
+        System.out.println("multiplication: 7 * 4 = " + 7 * 4);
+        System.out.println("division: 7 / 2 = " + 7 / 3);
 
     }
 }
