1c9be31097e7fab281d969f3020e068f76406354
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-14 16:08:27 +0300

    println and print methods in java

--- src/Day1/FirstJavaProgram.java
+++ src/Day1/FirstJavaProgram.java
@@ -2,6 +2,19 @@ package Day1;
 
 public class FirstJavaProgram {
     public static void main(String[] args) {
+
+        //using the println method
         System.out.println("Good Morning!!");
+        System.out.println("I am called Jack");
+        System.out.println("How are you??");
+        System.out.println("What is your name?");
+        System.out.println("\n");
+
+        //using print method
+        System.out.print("Hello");
+        System.out.print("Mark");
+
     }
 }
+
+
