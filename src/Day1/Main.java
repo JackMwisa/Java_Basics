33eeee4890e0d5f0c820f886b6424688b5b88f30
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-14 15:35:44 +0300

    file 2

--- src/Day1/Main.java
+++ src/Day1/Main.java
@@ -2,6 +2,8 @@ package Day1;
 
 //class is a container of functions
 public class Main {
+
+    //a method is a function that is part of class
     public static void main(String[] args) {
         System.out.println("Hello");
     }
