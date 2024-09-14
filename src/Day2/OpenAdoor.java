c9c790a9d123d5733a928d336de01ac77e421492
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-14 19:19:40 +0300

    Create OpenAdoor.java

---
+++ src/Day2/OpenAdoor.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class OpenAdoor {
+    public static void main(String[] args) {
+
+        short code = 12453;
+
+        String openDoor = (code == 12453) ? "Access granted" : "Wrong code";
+
+        System.out.println(openDoor);
+    }
+}
