f874861e4c77f2bf3680811831a2363da6f831b2
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-16 13:54:28 +0300

    Update ArraysInJava.java

--- src/Day3/ArraysInJava.java
+++ src/Day3/ArraysInJava.java
@@ -12,8 +12,9 @@ public class ArraysInJava {
 
         // length
         int[] hours = {8, 9, 54, 1,2,4,8,94};
-        System.out.println(hours.length);
-
         Arrays.sort(hours);
+        System.out.println("The length of my array is " + hours.length);
+        System.out.println(Arrays.toString(hours));
+
     }
 }
