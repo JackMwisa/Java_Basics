7d079b1984093baca25c9e4db578a07816c848b6
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-16 15:13:40 +0300

    Update Employee.java

--- src/Day3/Employee.java
+++ src/Day3/Employee.java
@@ -3,10 +3,23 @@ package Day3;
 public class Employee {
     public int baseSalary;
     public int hourlyRate;
-    public  int extraHours;
 
 
-    public int calculateWage() {
-        return baseSalary + hourlyRate + extraHours
+    public int calculateWage(int extraHours) {
+        return baseSalary + hourlyRate + extraHours;
     }
+
+    public static void main(String[] args) {
+        var employee1 = new Employee();
+
+        employee1.baseSalary = 50_000;
+        employee1.hourlyRate = 10;
+        int wage = employee1.calculateWage(10);
+
+        
+
+    }
+
 }
+
+
