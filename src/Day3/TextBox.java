8247ea29c9f2d1ae644732b6a87b0dc556a8c2f1
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-16 14:50:55 +0300

    Update TextBox.java

--- src/Day3/TextBox.java
+++ src/Day3/TextBox.java
@@ -1,5 +1,28 @@
 package Day3;
 
 public class TextBox {
-    public
+    public String text = "";
+
+//    Methods
+    public void setText(String myText){
+        this.text = myText;
+    }
+
+    public void clear() {
+        this.text = "";
+    }
+
+
+    public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box one");
+        System.out.println(textBox1.text);
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Box Two");
+        System.out.println(textBox2.text);
+
+    }
 }
+
+
