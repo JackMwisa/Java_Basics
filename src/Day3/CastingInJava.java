7731377187875b28dc69e422839385065a0ffa3e
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-16 14:33:05 +0300

    OOP

---
+++ src/Day3/CastingInJava.java
@@ -0,0 +1,11 @@
+package Day3;
+
+public class CastingInJava {
+    public static void main(String[] args) {
+        double x = 1.2;
+        int y = (int)x + 3;
+        System.out.println(y);
+
+
+    }
+}
--- src/Day3/constants.java
+++ src/Day3/constants.java
@@ -1,4 +1,8 @@
 package Day3;
 
 public class constants {
+    public static void main(String[] args) {
+        final float PI = 3.14f;
+        System.out.println(PI);
+    }
 }
