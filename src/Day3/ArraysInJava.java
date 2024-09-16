1c961419b8c2745f489351af6051e6ac0748bbcb
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-16 13:53:30 +0300

    Day3

--- src/Day3/ArraysInJava.java
+++ src/Day3/ArraysInJava.java
@@ -1,11 +1,19 @@
 package Day3;
 
+import java.util.Arrays;
+
 public class ArraysInJava {
     public static void main(String[] args) {
         int[] numbers = new int[5];
         numbers[0] = 1;
         numbers[1] = 2;
 
-        System.out.println(numbers.toString());
+        System.out.println(Arrays.toString(numbers));
+
+        // length
+        int[] hours = {8, 9, 54, 1,2,4,8,94};
+        System.out.println(hours.length);
+
+        Arrays.sort(hours);
     }
 }
