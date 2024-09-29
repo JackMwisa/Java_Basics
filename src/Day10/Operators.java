41999694274aef2e0cadf86924fa7081856cf30e
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-29 21:25:56 +0300

    Create Operators.java

---
+++ src/Day10/Operators.java
@@ -0,0 +1,16 @@
+package Day10;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        //Arithmetic
+        double x = 3;
+        double y = 2;
+        double z = x / y;
+        System.out.println(z);
+
+    }
+
+
+}
