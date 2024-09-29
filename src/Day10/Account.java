6ca5d494cafc4455b53d0cdebffc6d5572a61e2c
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-29 20:32:45 +0300

    Update Account.java

--- src/Day10/Account.java
+++ src/Day10/Account.java
@@ -4,6 +4,15 @@ public class Account {
     String owner;
     double balance;
     double minimumBalance;
+
+    public void deposit(double amount){
+        // implementation
+    }
+
+    public void withdraw(double amount){
+        // implementation
+    }
+
 }
 
 
