bf49daacc05f392d721ebeb3d1b4d8fabbbb470a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-29 20:21:53 +0300

    Create Hey.java

---
+++ src/Day9/Hey.java
@@ -0,0 +1,15 @@
+package Day9;
+
+import java.util.Scanner;
+
+public class Hey {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter yout Name? ");
+        String name = input.nextLine();
+        System.out.println("Hey, your name is " + name);
+
+
+    }
+}
