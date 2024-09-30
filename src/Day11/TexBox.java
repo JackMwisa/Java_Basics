d9efb16386880435e91678938a60671bf74f1dd1
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-30 09:53:28 +0300

    Update TexBox.java

--- src/Day11/TexBox.java
+++ src/Day11/TexBox.java
@@ -2,5 +2,15 @@ package Day11;
 
 public class TexBox {
     //fields or properties
-    
+    public String text;
+
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public void clear(){
+        this.text = "";
+    }
+
 }
