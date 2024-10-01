6d3660d3e573b76033ad481ac7d8b5006cec2900
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-01 14:10:02 +0300

    .

--- src/Day13/Main.java
+++ src/Day13/Main.java
@@ -1,7 +1,16 @@
 package Day13;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println(34);
+        System.out.println("How are you?");
+        Scanner input = new Scanner(System.in);
+        String answer = input.nextLine();
+        System.out.println("You said: " + answer);
+
+
+
+
     }
 }
