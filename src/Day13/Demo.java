5935c2a2f321938dae4330de8f38ff3d3b2c742a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-01 14:50:00 +0300

    update

--- src/Day13/Demo.java
+++ src/Day13/Demo.java
@@ -1,8 +1,16 @@
 package Day13;
 
+import java.util.Scanner;
+
 public class Demo {
     public static void main(String[] args) {
 
-        
+        Scanner input = new Scanner(System.in);
+
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        System.out.println(num1 + num2);
+
     }
 }
