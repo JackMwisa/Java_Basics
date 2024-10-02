ab2c06dcfcc7f8c6435e1757d3475ae939c32696
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-02 16:59:55 +0300

    done

---
+++ src/Day16/Animal.java
@@ -0,0 +1,9 @@
+package Day16;
+
+interface Animal {
+    void eat();
+    void drink();
+    void sleep();
+    void workout();
+}
+
---
+++ src/Day16/Main.java
@@ -0,0 +1,12 @@
+package Day16;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Milou");
+
+        myDog.eat();
+        myDog.drink();
+        myDog.sleep();
+        myDog.workout();
+    }
+}
