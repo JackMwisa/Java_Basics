bd9a2133844d9278bb2ad1ed2eaf27bafc708fb8
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-04 15:57:58 +0300

    Update Main.java

--- src/Day19/Question2/Main.java
+++ src/Day19/Question2/Main.java
@@ -2,20 +2,19 @@ package Day19.Question2;
 
 public class Main {
     public static void main(String[] args) {
-        var people = new Person[] {
-                new Man("John"),
-                new WorkerMan("Mike"),
-                new Woman("Anna"),
-                new WorkerWoman("Sara"),
-                new Man("David"),
-                new WorkerMan("Tom"),
-                new Woman("Eva"),
-                new WorkerWoman("Lily")
+        Person[] people = new Person[] {
+                new Man("Alex"),
+                new WorkerMan("Brian"),
+                new Woman("Catherine"),
+                new WorkerWoman("Diana"),
+                new Man("Edward"),
+                new WorkerMan("Frank"),
+                new Woman("Grace"),
+                new WorkerWoman("Hannah")
         };
 
-        for (var person : people) {
+        for (Person person : people) {
             person.work();
         }
     }
 }
-
