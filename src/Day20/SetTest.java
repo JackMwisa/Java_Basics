a83ca6a5a5f1dedeff744e8dd0e5803b778fb999
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-05 22:49:49 +0300

    HashSets

---
+++ src/Day20/SetTest.java
@@ -0,0 +1,29 @@
+package Day20;
+import java.util.*;
+
+public class SetTest {
+    public static void main(String[] args) {
+
+        // Create a HashSet to store the days of the week
+        Set<String> daysOfWeek = new HashSet<>();
+
+        // Add the days of the week to the HashSet
+        daysOfWeek.add("Monday");
+        daysOfWeek.add("Tuesday");
+        daysOfWeek.add("Wednesday");
+        daysOfWeek.add("Thursday");
+        daysOfWeek.add("Friday");
+        daysOfWeek.add("Saturday");
+        daysOfWeek.add("Sunday");
+
+
+        // Print the content of the HashSet
+        System.out.println("HashSet of days of the week: " + daysOfWeek);
+
+        // Create a LinkedHashSet to preserve insertion order
+        Set<String> orderedDays = new LinkedHashSet<>(daysOfWeek);
+
+        // Print the ordered Set
+        System.out.println("Ordered Set of days of the week: " + orderedDays);
+    }
+}
