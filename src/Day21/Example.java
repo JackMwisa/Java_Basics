b7ddf8f0476ab2a46bd1307d70ed7119557a2232
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-07 14:39:49 +0300

    Update Example.java

--- src/Day21/Example.java
+++ src/Day21/Example.java
@@ -1,9 +1,6 @@
 package Day21;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Example {
     public static void main(String[] args) {
@@ -19,13 +16,18 @@ public class Example {
 
 
 
-        Set<Integer> numbers = new HashSet<>();
+        Set<Integer> numbers = new LinkedHashSet<>();
         numbers.add(1);
+        numbers.add(4);
         numbers.add(2);
         numbers.add(2);
         numbers.add(3);
 
         System.out.println(numbers);
+
+        for (int num : numbers){
+            System.out.println(num);
+        }
     }
 
 }
