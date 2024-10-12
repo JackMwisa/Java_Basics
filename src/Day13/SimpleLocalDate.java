ae8d534c9f02252bd32ae2564fd52765301d8f6b
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-13 02:41:19 +0300

    localdata in java

---
+++ src/Day13/SimpleLocalDate.java
@@ -0,0 +1,16 @@
+package Day13;
+
+import java.time.LocalDate;
+
+public class SimpleLocalDate {
+    public static void main(String[] args) {
+
+        LocalDate ld = LocalDate.now();
+        LocalDate ld1 = LocalDate.of(2000, 5, 3);
+        LocalDate ld2 = LocalDate.parse("2023-01-02");
+
+        System.out.println(ld);
+        System.out.println(ld1);
+        System.out.println(ld2);
+    }
+}
