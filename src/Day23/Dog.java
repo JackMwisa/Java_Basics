0a32a8ee62634bad1d7aa308310bb11f99683d37
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-12 11:10:58 +0300

    Create Dog.java

---
+++ src/Day23/Dog.java
@@ -0,0 +1,14 @@
+package Day23;
+
+public class Dog extends SimpleAnimal {
+    public void bark() {
+        System.out.println("The dog is barking!");
+    }
+}
+
+public class Cat extends SimpleAnimal {
+    public void meow() {
+        System.out.println("The cat is meowing!");
+    }
+}
+
