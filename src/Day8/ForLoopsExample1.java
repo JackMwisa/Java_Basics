5cef592e0478593a4a439149d66514353a31a1ba
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-12 15:58:42 +0300

    Create ForLoopsExample1.java

---
+++ src/Day8/ForLoopsExample1.java
@@ -0,0 +1,18 @@
+package Day8;
+
+public class ForLoopsExample1 {
+    public static void main(String[] args) {
+        int[] nrs = {1,2,3,4,5,6,7,8,9,10};
+        int search = 7;
+        int index = -1;
+
+
+        for (int i = 0; i < nrs.length; i++) {
+            if (nrs[i] == search) {
+                index = i;
+            }
+        }
+
+        System.out.println(index);
+    }
+}
