184a220dfc619f775b2063c3147c0faca90ecae3
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-12 17:14:26 +0300

    Update Person.java

--- src/Day24/Person.java
+++ src/Day24/Person.java
@@ -3,15 +3,13 @@ package Day24;
 public class Person {
 
     public String name;
-    private String secret;
-
+    protected String secret;
 
 
     public void sayHi(){
         System.out.println("Hello, I'm " + name);
     }
-
-
+    
     private void tellSecret() {
         System.out.println("the secret is " + secret);
 
