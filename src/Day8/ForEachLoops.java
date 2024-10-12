7f594507f3f780039bed74f971508919acecbaa8
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-12 16:06:02 +0300

    ForEachLoops

---
+++ src/Day8/ForEachLoops.java
@@ -0,0 +1,13 @@
+package Day8;
+
+public class ForEachLoops {
+    public static void main(String[] args) {
+
+        String[] names = {"Supra", "Bob", "John"};
+
+        for (String name : names) {
+            System.out.println(name);
+
+        }
+    }
+}
