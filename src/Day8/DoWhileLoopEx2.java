fbfb4e65f19ad80c30863f3910a0a83ecc8fdb5d
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-12 15:44:59 +0300

    Update DoWhileLoopEx2.java

--- src/Day8/DoWhileLoopEx2.java
+++ src/Day8/DoWhileLoopEx2.java
@@ -1,4 +1,18 @@
 package Day8;
 
+import java.util.Scanner;
+
 public class DoWhileLoopEx2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String message;
+
+        do {
+            System.out.print("Enter your name: ");
+            message = input.nextLine();
+        } while (message.length() < 2);
+
+    }
 }
