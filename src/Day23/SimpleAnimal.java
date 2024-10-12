e75874760638844aef46015ad174604d0d79c19d
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-12 11:10:01 +0300

    abstract class SimpleAnimal

---
+++ src/Day23/SimpleAnimal.java
@@ -0,0 +1,29 @@
+package Day23;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class SimpleAnimal implements AnimalInteraction {
+    protected List<String> interactionHistory = new ArrayList<>();
+
+    @Override
+    public void feed(String food) {
+        interactionHistory.add("Fed with: " + food);
+    }
+
+    @Override
+    public void play() {
+        interactionHistory.add("Played");
+    }
+
+    @Override
+    public void showInteractionHistory() {
+        for (String interaction : interactionHistory) {
+            System.out.println(interaction);
+        }
+    }
+
+    public void sleep(int hours) {
+        interactionHistory.add("Slept for " + hours + " hours");
+    }
+}
+
