55e7e772d6f629e72d5f15b54dd9398572f2abac
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-14 21:06:32 +0300

    Create ExampleMaps.java

---
+++ src/Week4/Day26/ExampleMaps.java
@@ -0,0 +1,30 @@
+package Week4.Day26;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExampleMaps {
+    public static void main(String[] args) {
+        HashMap<String, Integer> ourMealsPrices = new HashMap<>();
+
+        ourMealsPrices.put("Pizza", 57);
+        ourMealsPrices.put("Fruits", 90);
+        ourMealsPrices.put("Cakes", 76);
+
+        System.out.println(ourMealsPrices);
+
+        System.out.println(ourMealsPrices.keySet());
+        System.out.println(ourMealsPrices.values());
+
+        ArrayList<String> ourMealsNames = new ArrayList<>();
+        ourMealsNames.add("Pizza");
+        ourMealsNames.add("Fruits");
+        ourMealsNames.add("Cakes");
+
+        System.out.println(ourMealsNames);
+
+
+    }
+
+}
