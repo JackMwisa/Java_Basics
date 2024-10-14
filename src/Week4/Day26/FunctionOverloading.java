d98ca036dc91ade11f36fb1c46d2f4428c8ce2cc
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-14 20:21:10 +0300

    function overloading

---
+++ src/Week4/Day26/FunctionOverloading.java
@@ -0,0 +1,42 @@
+package Week4.Day26;
+
+public class FunctionOverloading {
+    public static void main(String[] args) {
+
+        fun(45);
+
+        System.out.println();
+
+        fun("Mwisa Kassanova Jean-Jacques");
+
+        System.out.println();
+
+        int sum1 = sum(3,2);
+        System.out.println("First sum function " + sum1);
+
+        System.out.println();
+
+        int sum2 = sum(1,4,6);
+        System.out.println("Second sum function " + sum2);
+    }
+
+    static void fun(int a){
+        System.out.println("Frist Function");
+        System.out.println(a);
+    }
+
+    static void fun(String name){
+        System.out.println("Second Function");
+        System.out.println(name);
+    }
+
+
+    static int sum(int a, int b){
+        return a + b;
+    }
+
+    static int sum(int a, int b, int c){
+        return a + b+ c;
+    }
+
+}
--- src/Week4/Day26/VarbsArgs.java
+++ src/Week4/Day26/VarbsArgs.java
@@ -8,9 +8,13 @@ public class VarbsArgs {
         fun(9,8,7,6,5,4,3,2,1);
 
         fun(1,2,3,4,5,6,7,8,9);
+
+        multiple(3,5, "John", "Paul", "Marrot");
     }
 
-    static void m
+    static void multiple(int a, int b, String ...c){
+        System.out.println(a + ", " + b +  ", " + Arrays.toString(c));
+    }
 
     static void fun(int ...v){
         System.out.println(Arrays.toString(v));
