1db0af4ba5d110e8e08ea7be2a78fa3e14b8bc7a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-20 17:57:15 +0300

    .

---
+++ src/Week4/Day26/OOP3/Chihuahua.java
@@ -0,0 +1,13 @@
+package Week4.Day26.OOP3;
+
+public class Chihuahua {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Week4/Day26/OOP3/ChihuahuaBag.java
@@ -0,0 +1,13 @@
+package Week4.Day26.OOP3;
+
+public class ChihuahuaBag {
+    private Chihuahua chihuahuaContent;
+
+    public Chihuahua getChihuahuaContent() {
+        return chihuahuaContent;
+    }
+
+    public void setChihuahuaContent(Chihuahua chihuahuaContent) {
+        this.chihuahuaContent = chihuahuaContent;
+    }
+}
