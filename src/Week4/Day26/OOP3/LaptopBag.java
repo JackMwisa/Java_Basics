bc33dbaf89b31d7b743222c7764c4d2bac8f2759
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-20 17:55:43 +0300

    generics and collections

---
+++ src/Week4/Day26/OOP3/LaptopBag.java
@@ -0,0 +1,13 @@
+package Week4.Day26.OOP3;
+
+public class LaptopBag {
+    private Laptop laptopContent;
+
+    public Laptop getLaptopContent() {
+        return laptopContent;
+    }
+
+    public void setLaptopContent(Laptop laptopContent) {
+        this.laptopContent = laptopContent;
+    }
+}
