ab5096a84447971437c5b4443233e324a84cc76d
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-11-11 14:32:03 +0300

    .

--- src/Week5/Day27/Greet.java
+++ src/Week5/Day27/Greet.java
@@ -3,8 +3,22 @@ package Week5.Day27;
 
 public class Greet{
 
-    
+    // fields  - properties  of the class
 
+    public String name;
+    public int age;
+    public String message;
+
+
+    public Greet(int  age, String name) {
+
+        this.age = age;
+        this.name = name;
+    }
+
+
+
+    // attributes - features  of the class
 
 
     public void sayHello(){
@@ -26,5 +40,19 @@ public class Greet{
 }
 
 
+
+/**
+ * 
+ * We are creating a greet class
+ * it has 3 properties 
+ * name
+ * age
+ * message
+ * 3 functions
+ * - sayHello
+ * -sayBye
+ * - how old are u? 
+ * 
+ */
 
 
--- src/Week5/Day27/Main.java
+++ src/Week5/Day27/Main.java
@@ -4,12 +4,17 @@ package Week5.Day27;
 public class Main{
     public static void main(String[] args) {
 
+        Greet person1 = new Greet(30, "John");
+        Greet person2  = new Greet(25, "Jane");
+        Greet person3 = new Greet(45, "Mark");
+
+
+        person1.sayBye();
+        person2.sayHello();
 
 
 
 
-
-        
 
     }
 
