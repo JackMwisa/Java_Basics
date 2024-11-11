afa402402043b444c30027b62dbe02e2c4c94558
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-11-11 13:44:22 +0300

    commit 2

---
+++ src/Week5/Day27/Greet.java
@@ -0,0 +1,30 @@
+package Week5.Day27;
+
+
+public class Greet{
+
+    
+
+
+
+    public void sayHello(){
+        System.out.println("Hello");
+
+    }
+
+
+    public void sayBye(){
+        System.out.println("Bye");
+
+    }
+
+
+    public int howOldAreYou(int age){
+        return age;
+    }
+
+}
+
+
+
+
---
+++ src/Week5/Day27/Main.java
@@ -0,0 +1,17 @@
+package Week5.Day27;
+
+
+public class Main{
+    public static void main(String[] args) {
+
+
+
+
+
+
+        
+
+    }
+
+
+}
