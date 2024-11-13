507d18492173ac9b64d969fe017d59600225327a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-11-13 11:56:49 +0300

    Update PrimitiveDataTypes.java

--- src/Week5/Day27/PrimitiveDataTypes.java
+++ src/Week5/Day27/PrimitiveDataTypes.java
@@ -1,4 +1,26 @@
 package Week5.Day27;
 
 public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        // Declare variables
+        int width;
+        int height;
+        int area;
+        double radius = 10.0;
+        double pi = 3.14;
+        boolean result = true;
+
+        // Assign values
+        width = 8;
+        height = 12;
+        area = 96;
+
+        // Print values
+        System.out.println("width: " + width);
+        System.out.println("height: " + height);
+        System.out.println("area: " + area);
+        System.out.println("radius: " + radius);
+        System.out.println("pi: " + pi);
+        System.out.println("result: " + result);
+    }
 }
